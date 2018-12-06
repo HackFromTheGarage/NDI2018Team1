a739a6e0fd05e6a0e1c73c5012195bba8b78700e
Jules Ponsaillé <jules.ponsaille@isep.fr>
2018-12-06 22:18:35 +0100

    change routes for bot

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const api = require('./api/index.js')
 
 
 nextApp.prepare().then(() => {
-    const app = express()
+    const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use('/api', api);
