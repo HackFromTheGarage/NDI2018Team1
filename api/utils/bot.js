10f74dff061abc6e77a032bd8014e1cfa64c0d5e
gab <gabriel.mougard@gmail.com>
2018-12-07 04:25:06 +0100

    bot.js - formattedAnswer

--- api/utils/bot.js
+++ api/utils/bot.js
@@ -7,6 +7,11 @@ function generateAnswer(message) {
   const words = message.split(" ").filter((word) => word.length>2);
   let similarityArray = [];
 
+  const insult = formattedAnswer(message)
+  if (insult) {
+    return insult;
+  }
+
   const resFinal = words.map(element => {
       Object.keys(json).forEach( key => {
         similarityArray.push([w2v.similarity(element,key), key]);
@@ -15,7 +20,7 @@ function generateAnswer(message) {
       return similarityArray[0];
   });
 
-  return SentimentAnalysis(resFinal,message);
+  return SentimentAnalysis(resFinal[1],message);
 
 }
 
@@ -30,5 +35,19 @@ function SentimentAnalysis(topic,text) {
   const size = quotes.length();
   const r = random(min = 0, max = size -1);
   return quotes[r];
+
+}
+
+function formattedAnswer(text) {
+  var answer
+
+  const swrWords = ["arse","arsehole","arses","ass","asses","asshole","bastard","bitch","bloody","boob","butt","butts","cock","cocks","crap","crappy","cunt","damn","dang","darn","dick","dicks","dumb","dyke","fuck","fucked","fucker","fuckin","fucks","goddam","heck","hell","homo","jeez","mofo","motherf","nigger","piss","prick","pussy","queer","screw","shit","sob","sonofa","suck","sucked","sucks","tit","tits","titties","titty","wanker"];
+  swrWords.forEach(word => {
+    if (text.indexOf(word) !== -1) {
+      answer = "Wow, you said '" + word +"', didn't you? Your mom will be really proud of you!"
+      return answer
+    }
+  });
+  return false
 
 }
