4404289ffd09744c4d1dfcb807b4ffd51f820e8a
Jules Ponsaillé <jules.ponsaille@isep.fr>
2018-12-07 01:23:22 +0100

    change server

--- api/index.js
+++ api/index.js
@@ -2,8 +2,19 @@ const { Router } = require('express')
 
 const router = Router()
 
-const indexHandler = require('./routes/index.js')
+router.post('/bot/sendMessage', function (req, res, next) {
+    const { message } = req.body;
 
-router.use('/', indexHandler)
+    return res.json({
+        "message": message
+    })
+})
+
+router.get('/', function (req, res, next) {
+    return res.json({
+        "name": "ndihackfromthegarage1",
+        "version": "1.0.0"
+    })
+})
 
 module.exports = router
