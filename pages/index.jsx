7c3ed94ef05acac06c8684ba3237e1ba985de9d1
Unknown <sebastien.mascha@icloud.com>
2018-12-07 05:53:16 +0100

    Mise a jour

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,30 @@
 import Layout from '../layouts/default';
 export default () => (
-    <Layout title="Acueil | HackFromTheGarage1">
-       <p>Projet de HackFromTheGarage1</p>
-    </Layout>
+    <div style={{textAlign:"center"}}>
+        <Layout title="Acueil | HackFromTheGarage1">
+           <p>Projet de HackFromTheGarage1</p>
+            <div>
+                <div className="rectangle"></div>
+                <div className="rectangle"></div>
+
+            </div>
+            <div>
+                <div className="rectangle"></div>
+                <div className="rectangle"></div>
+            </div>
+
+        </Layout>
+
+        <style jsx>{`
+            .rectangle{
+                width:200px;
+                height:200px;
+                border-radius:5px;
+                border: 1px solid black;
+                margin:5px;
+            }
+
+        `}</style>
+    </div>
+
 )
